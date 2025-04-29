dcebb2daca35aba65e6f6d60ec084ee9df3ee07d
henriqueaaa <henrique.santiago@edu.unifil.br>
2025-04-29 13:02:58 -0300

    Create Modulo.js

---
+++ Modulo.js
@@ -0,0 +1,12 @@
+/* Estudo dos operadores 
+
+Módulo (a % b) a = 8 b = 11
+
+O cálculo do módulo se da pelo resultado da multiplicação entre o quociente e o divisor.
+
+8 / 11 = 0.72... 
+
+
+
+*/
+console.log(8 / 11 * 11)
