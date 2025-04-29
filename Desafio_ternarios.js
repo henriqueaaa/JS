3a520783c7bc7a8871d1f949c3c22d27e499f16a
henriqueaaa <henrique.santiago@edu.unifil.br>
2025-04-29 13:01:52 -0300

    Create Desafio_ternarios.js

---
+++ Desafio_ternarios.js
@@ -0,0 +1,9 @@
+const a = 40;
+const b = 4;
+const operacao = '/'; // + - */
+
+const resultado = 
+     operacao === '+' ? a + b : 
+        operacao === '-'? a - b : 
+           operacao === '*' ? a * b : a / b;
+console.log(resultado);
