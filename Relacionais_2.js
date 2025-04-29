44da4e329215022b031ba79e90d0f63b7de413d5
henriqueaaa <henrique.santiago@edu.unifil.br>
2025-04-29 13:04:14 -0300

    Create Relacionais_2.js

---
+++ Relacionais_2.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = "1";
+let c = "1";
+
+console.log(a == b);
+console.log(a === b); // comparação estrita (comparação de tipos)
+console.log(b === c);
+
+console.log(a != b);
+console.log(a !== b);
+console.log(b !== c);
+
+
