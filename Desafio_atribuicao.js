1666b76811b6d98d00cdfc3faed95f5beb426d7d
henriqueaaa <henrique.santiago@edu.unifil.br>
2025-04-29 13:00:38 -0300

    Create Desafio_atribuicao.js

---
+++ Desafio_atribuicao.js
@@ -0,0 +1,24 @@
+let  = 'Bom dia!\nTudo bem?'; // \n nova linha//
+
+console.log();
+
+/* 
+Lista de Aprovados
+==================
+1. Ana Silva
+2. Pedro Albuquerque
+3. Guilherme Pereira 
+4. Rebeca França
+*/
+
+let texto = 'Lista de Aprovados\n';
+texto += '==================\n\n';
+texto += '1. Bernardo ruimzasso\n'
+texto += '2. Pedro Albuquerque\n'
+texto += '3. Guilherme Pereira\n'
+texto += '4. Rebeca França\n'
+
+console.log(texto)
+
+
+
