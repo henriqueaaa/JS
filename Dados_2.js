cf507c5d69cad7df96f7c42c2ddbb618ff8bbcc4
henriqueaaa <henrique.santiago@edu.unifil.br>
2025-04-29 12:51:06 -0300

    Create Dados_2.js

---
+++ Dados_2.js
@@ -0,0 +1,15 @@
+// Variavel var(ou let) + identificador(definir bons nomes)
+
+console.log(19.90 * 0.6);
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComdesconto = preco * (1 - desconto);
+console.log(precoComdesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
